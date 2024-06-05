931ab24f5d4d143b9e27df373b4ea1b673785e10
Luis Rosero <luis.rosero@wiga.io>
2024-06-04 23:32:20 -0500

    Feat: correcion de la rama

--- apiDigiTurnoApp/prisma/migrations/20240604024119_second/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `LUGAR` to the `TURNOS` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TURNOS" ADD COLUMN     "LUGAR" TEXT NOT NULL;
